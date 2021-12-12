36f37245a211e561fde6acdb19cb79f1e7a7a2f3
Pomarancz <kuba.wtorkiewicz@gmail.com>
2021-12-12 19:51:43 +0100

    gówno działa

--- wyplaty/main.cpp
+++ wyplaty/main.cpp
@@ -1,14 +1,42 @@
-//
-//  main.cpp
-//  wyplaty
-//
-//  Created by Kuba Wtorkiewicz on 12/12/2021.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int nominaly[5]={200,100,50,20,10};
+    int wyplaty[10]={0};
+    int kasa[10][5]={0};
+    int bank[5]={0};
+    
+    
+    for (int i=0; i<10; i++) {
+        cout << "ile zarabia pracownik " << i+1 << " ?" << endl;
+        cin >> wyplaty[i];
+    }
+    
+    for (int i=0; i<10; i++)
+    {
+        int temp=wyplaty[i];
+        for (int j=0; j<5; j++)
+        {
+            kasa[i][j]=temp/nominaly[j];
+            temp=temp%nominaly[j];
+        }
+    }
+    
+    
+    for (int j=0; j<5; j++)
+    {
+        for (int i=0; i<10; i++)
+        {
+            bank[j]+=kasa[i][j];
+        }
+    }
+    
+    for (int i=0;i<5;i++)
+    {
+        cout << "Nalezy wyplacic " << bank[i] << " o nominale " << nominaly[i] << endl;
+    }
+    
+    
     return 0;
 }
